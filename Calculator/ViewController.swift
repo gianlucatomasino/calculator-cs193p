a8ef002a94ea693f9c5ab3c619dd9f9a0f385e46
Gianluca Tomasino <gianluca.tomasino@jobrapido.com>
2015-10-13 10:26:25 +0200

    First version

---
+++ Calculator/ViewController.swift
@@ -0,0 +1,103 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Gianluca Tomasino on 04/02/15.
+//  Copyright (c) 2015 Gianluca Tomasino. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var display: UILabel!
+
+    var userIsInTheMiddleOfTypingANumber = false;
+    var userHasPressedPeriod = false
+    
+    @IBAction func appendDigit(sender: UIButton) {
+        let digit = sender.currentTitle!
+        
+        if userIsInTheMiddleOfTypingANumber {
+            display.text = display.text! + digit
+        } else {
+            display.text = digit
+            userIsInTheMiddleOfTypingANumber = true
+        }
+    }
+    
+    @IBAction func operate(sender: UIButton) {
+        let operation = sender.currentTitle!
+        
+        if userIsInTheMiddleOfTypingANumber {
+            enter()
+        }
+        
+        switch operation {
+            case "×": performOperation { $0 * $1 }
+            case "−": performOperation { $1 - $0 }
+            case "+": performOperation { $0 + $1 }
+            case "÷": performOperation { $1 / $0 }
+            case "√": performOperation {sqrt($0)}
+            case "sin" : performOperation {sin($0)}
+            case "cos": performOperation {cos($0)}
+            default: break
+        }
+    }
+    
+    @IBAction func pi() {
+        if (userIsInTheMiddleOfTypingANumber) {
+            enter()
+        }
+        
+        display.text = "3.14"
+        enter()
+    }
+    
+    private func performOperation(operation: (Double, Double) -> Double) {
+        if operandStack.count >= 2 {
+            displayValue = operation(operandStack.removeLast(), operandStack.removeLast())
+            enter()
+        }
+    }
+    
+    private func performOperation(operation: Double -> Double) {
+        if operandStack.count >= 1 {
+            displayValue = operation(operandStack.removeLast())
+            enter()
+        }
+    }
+    
+    @IBAction func period() {
+        if !userHasPressedPeriod {
+            if userIsInTheMiddleOfTypingANumber {
+                display.text = display.text! + "."
+            } else {
+                display.text = "0."
+            }
+            userHasPressedPeriod = true
+            userIsInTheMiddleOfTypingANumber = true
+        }
+    }
+    
+    
+    var operandStack = Array<Double>()
+    
+    @IBAction func enter() {
+        userIsInTheMiddleOfTypingANumber = false
+        userHasPressedPeriod = false
+        operandStack.append(displayValue)
+        print("operandStack = \(operandStack)")
+    }
+    
+    var displayValue: Double {
+        get {
+            return NSNumberFormatter().numberFromString(display.text!)!.doubleValue
+        }
+        set {
+            display.text = "\(newValue)"
+            userIsInTheMiddleOfTypingANumber = false
+        }
+    }
+}
+
